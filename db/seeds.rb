cf7d5ac3390ddb2c0e4276f87d2b29bbc1dbaa22
Adam Maślanka <h4b00@users.noreply.github.com>
2021-07-20 13:38:53 +0200

    Add recipe seeds (#7)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+%w[Fried\ Zucchini Apple\ Cider\ Donuts Homemade\ Pumpkin Pie\ Spice].each do |meal_name|
+  Recipe.create(name: meal_name, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Aenean condimentum risus dui, volutpat consequat enim auctor quis. Sed eu elit egestas,
+    scelerisque quam quis, posuere libero. Fusce tincidunt sapien vel efficitur cursus.
+    Nam a pretium purus, vel vestibulum mi. Aliquam erat volutpat. Morbi sed fermentum ante. Donec maximus lacinia libero,
+    tempor semper libero ultrices posuere. Maecenas id erat ac enim pharetra consequat.")
+end
